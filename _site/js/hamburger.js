eb1415e5f3adbe5f2549f69a29963ff371ea1015
lcchand <lcchandgithub@gmail.com>
2020-01-11 16:23:06 -0500

    CSS: Remove Old Commented code

--- _site/js/hamburger.js
+++ _site/js/hamburger.js
@@ -2,7 +2,6 @@ var sidebarHamburger = document.getElementById('hamburger');
 var HBsidebar = document.getElementById('sidebar');
 
 sidebarHamburger.onclick = function() {
-    //var HBsidebar = document.getElementById('sidebar');
     
     if (HBsidebar.style.display === "") {
 	 HBsidebar.style.display = "block";
